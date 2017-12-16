2242603fd651dab69efe920a83d56058793aa14b
Eduardo Urzua <eduardo.urzua07@inacapmail.cl>
2017-12-15 21:45:06 -0300

    merge resolve conflict

--- app/src/main/java/chilerifa/nicolasgh/com/chilerifa/PrizeDetailsActivity.java
+++ app/src/main/java/chilerifa/nicolasgh/com/chilerifa/PrizeDetailsActivity.java
@@ -1,21 +1,23 @@
 package chilerifa.nicolasgh.com.chilerifa;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class PrizeDetailsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prize_details);
         backButton();
+
     }
 
     protected void backButton(){
-        Button backButtonCar = findViewById(R.id.btn_back_car);
+        Button backButtonCar = findViewById(R.id.btn_back);
         backButtonCar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +26,7 @@ public class PrizeDetailsActivity extends AppCompatActivity {
         });
 
     }
+
+
 
 }
--- app/src/main/java/chilerifa/nicolasgh/com/chilerifa/PrizesActivity.java
+++ app/src/main/java/chilerifa/nicolasgh/com/chilerifa/PrizesActivity.java
@@ -4,26 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PrizesActivity extends AppCompatActivity {
+    ListView prizeList;
+    String[] prizename = {"Tv","Car","Trip","Laptop","Pony"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prizes);
-        prizeClick();
+
+        prizeList = findViewById(R.id.prize_list);
+        PrizeClicks();
+
     }
 
-    private void prizeClick() {
-        Button prizeButton = findViewById(R.id.btn_prize_tv);
-        prizeButton.setOnClickListener(new View.OnClickListener() {
+
+    public void PrizeClicks(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,prizename);
+        prizeList.setAdapter(adapter);
+        prizeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 startActivity(new Intent(PrizesActivity.this,PrizeDetailsActivity.class));
             }
         });
 
     }
+
+
+
     }
 
